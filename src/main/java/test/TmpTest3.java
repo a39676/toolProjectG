6bd743c36e56ede747acfe9aef130a2c3d0ee684
a39676 <396767101@qq.com>
2018-10-02 21:33:01 +0800

    .

--- src/main/java/test/TmpTest3.java
+++ src/main/java/test/TmpTest3.java
@@ -1,20 +1,13 @@
 package test;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.util.UUID;
 
 public class TmpTest3 {
 	
 	public static void main(String[] args) throws IOException {
 		
-		String sourceFolderPath = "g:/imageCache/notPostYet/1/";
-		
-		File folder = new File(sourceFolderPath);
-		List<File> files = Arrays.asList(folder.listFiles());
-		
-		files.stream().filter(f -> f.getName().contains("(1)")).forEach(f -> System.out.println(f.getName()));
+		System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
 		
 	}
 
