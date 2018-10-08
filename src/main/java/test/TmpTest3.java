f5e0b2e451a4c432147c9f64e27e5d99b387f315
a39676 <396767101@qq.com>
2018-10-08 22:39:01 +0800

    .

--- src/main/java/test/TmpTest3.java
+++ src/main/java/test/TmpTest3.java
@@ -7,7 +7,7 @@ public class TmpTest3 {
 	
 	public static void main(String[] args) throws IOException {
 		
-		System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
+		System.out.println(UUID.randomUUID().toString().replaceAll("-", "").replaceAll("a", "["));
 		
 	}
 
