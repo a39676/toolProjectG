44b8d408300313271347ac4558f2aa72839731f4
a39676 <396767101@qq.com>
2019-01-24 15:56:47 +0800

    jar source change

--- src/main/java/test/TmpTest3.java
+++ src/main/java/test/TmpTest3.java
@@ -1,40 +1,10 @@
 package test;
 
-import java.io.IOException;
-import java.util.UUID;
-
 public class TmpTest3 {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
-		TestA ab = new TestB();
-		ab = new TestB();
-		System.out.println(ab);
-		
-		System.out.println(UUID.randomUUID().toString().replaceAll("-", "").replaceAll("a", "["));
-		System.out.println(System.currentTimeMillis());
 		
 	}
-
-}
-
-class TestA {
 	
-	static {
-		System.out.println("1");
-	}
-	
-	public TestA() {
-		System.out.println("2");
-	}
-}
-
-class TestB extends TestA {
-	static {
-		System.out.println("x");
-	}
-	
-	public TestB() {
-		System.out.println("y");
-	}
 }
